e916bb1cfba7df8d351a788fb4bdffa68860c112
PaCHoN <juan@randombitsonfire.com>
2023-08-11 14:30:10 +0200

    repos

--- database/src/main/java/com/inditex/visibilidad/database/usecase/getvisibilidad/GetVisibilidadAdapter.java
+++ database/src/main/java/com/inditex/visibilidad/database/usecase/getvisibilidad/GetVisibilidadAdapter.java
@@ -2,15 +2,25 @@ package com.inditex.visibilidad.database.usecase.getvisibilidad;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.inditex.visibilidad.core.usecase.getvisibilidad.GetVisibilidadPort;
+import com.inditex.visibilidad.database.repositories.ProductRepository;
+import com.inditex.visibilidad.database.repositories.SizeRepository;
+import com.inditex.visibilidad.database.repositories.StockRepository;
 
 @Service
 public class GetVisibilidadAdapter implements GetVisibilidadPort {
 	
-//	@Autowired
-//    private VisibilidadRepository priceRepository;
+	@Autowired
+    private ProductRepository productRepository;
+	
+	@Autowired
+    private SizeRepository sizeRepository;
+    
+    @Autowired
+    private StockRepository stockRepository;
 
 	@Override
 	public List<Long> getVisibilidad() {
